8710a1a5b4eeb84cf0ba33330589cb9baac7ba6d
Theresa Manney <theresa.m.17@gmail.com>
2017-06-13 16:35:28 -0700

    Things should be showing by now but are not

--- src/views/movies_view.js
+++ src/views/movies_view.js
@@ -8,7 +8,24 @@ import Movie from '../models/movie.js';
 var MoviesView = Backbone.View.extend({
   initialize: function(params) {
     this.template = params.resultTemplate;
-    this.detailsTemplate = params.
+    this.detailsTemplate = params.stockTemplate;
+    this.listenTo(this.model, 'update', this.render);
+  },
+  render: function() {
+    this.$('#movie-results').empty();
+    this.$('#movies-stocked').empty();
+
+    var that = this;
+
+    this.model.each(function(movie) {
+      console.log(movie);
+      var movieView = new MovieView({
+        model: movie,
+        template: that.template
+      });
+      that.$('#movie-results').append(movieView.render().$el);
+    });
+    return this;
   }
 });
 
