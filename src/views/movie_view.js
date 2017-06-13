ebec40f54c7f9982db85593bea630ab66582150d
Cara Comfort <cara.comfort10@gmail.com>
2017-06-13 15:19:12 -0700

    Add MovieView with initialize and render functions

---
+++ src/views/movie_view.js
@@ -0,0 +1,18 @@
+import Backbone from 'backbone';
+import $ from 'jquery';
+
+var MovieView = Backbone.View.extend({
+  tagName: 'li',
+  initialize: function(params) {
+    this.template = params.template;
+    // this.listenTo(this.model, 'change', this.render);
+  },
+
+  render: function() {
+    var movieHTML = this.template({movie: this.model.attributes});
+    this.$el.html(movieHTML);
+    return this;
+  }
+});
+
+export default MovieView;
