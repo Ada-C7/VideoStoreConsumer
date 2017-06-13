6a5de0a9ac7aa5bd938f7fc626027d3f631e33d6
Danielle Birbal <birbalds@gmail.com>
2017-06-13 15:13:28 -0700

    Some views/models

--- src/app.js
+++ src/app.js
@@ -5,13 +5,17 @@ import $ from 'jquery';
 import _ from 'underscore';
 
 import MovieList from 'collections/movie-list';
-// import MovieListView from '/views/movie_list_view';
+import MovieListView from '/views/movie_list_view';
 
 var movieList = new MovieList();
+var movielistView = new MovieListView({
+  model: movieList,
+  template: _.template($('#movie-list-template').html()),
+  el: '.main-content'
+});
 // ready to go
 $(document).ready(function() {
 
-  console.log(movieList.model);
   $('section.main-content').append('<p>Hello World!</p>');
 
 });
