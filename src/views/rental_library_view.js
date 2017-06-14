002d5c8e3f7a20287eff37e08c1968944afc7900
Ann Dai <gofarann@gmail.com>
2017-06-14 11:32:49 -0700

    rental library setup with search function

--- src/views/rental_library_view.js
+++ src/views/rental_library_view.js
@@ -4,5 +4,17 @@ import $ from 'jquery';
 
 import RentalLibrary from '../collections/rental_library';
 
+var RentalLibraryView = Backbone.View.extend({
+  initialize: function(params) {
+    this.template = params.template;
+    this.listenTo(this.model, "update", this.render);
+  },
+
+  events: {
+    "click #search": "search"
+  }
+
+});
+
 
 export default RentalLibraryView;
