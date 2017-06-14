4cf3b60c2908315e0e73bc153c3281a66ce5ff65
Tamiko Terada <tamikodevo@gmail.com>
2017-06-14 14:31:25 -0700

    List of Movies show on screen.
    Created .render() for both Movie and MovieList views.
    Created .addMovie() callback in MovieList to initialize Movie views.

--- src/views/movie_list_view.js
+++ src/views/movie_list_view.js
@@ -1,10 +1,45 @@
 import $ from 'jquery';
 import _ from 'underscore';
 import Backbone from 'backbone';
-// import
+
+import MovieView from 'views/movie_view';
 
 var MovieListView = Backbone.View.extend({
-  
+  initialize: function(params) {
+    var self = this;
+
+    this.movieListTemplate = params.movieListTemplate;
+    this.movieTemplate = params.movieTemplate;
+
+    this.movieViewList = [];
+
+    this.model.forEach(function(movieData) {
+      self.addMovie(movieData);
+    });
+
+    this.listenTo(this.model, 'add', this.addMovie);
+    this.listenTo(this.model, 'update', this.render);
+  },
+
+  render: function() {
+    var self = this;
+    this.$('#movie-list').empty();
+
+    this.movieViewList.forEach(function(movieView) {
+      movieView.render();
+      self.$('#movie-list').append(movieView.$el); // exp w/deleting el
+    });
+    return this;
+  },
+
+  addMovie: function(movie) {
+    var movieView = new MovieView( {
+      model: movie,
+      movieListTemplate: this.movieListTemplate,
+    });
+
+    this.movieViewList.push(movieView);
+  },
 });
 
 export default MovieListView;
--- src/views/movie_view.js
+++ src/views/movie_view.js
@@ -1,7 +1,18 @@
 import Backbone from 'backbone';
 
 var MovieView = Backbone.View.extend({
-  initialize: function(options) {
-
+  initialize: function(params) {
+    this.movieListTemplate = params.movieListTemplate;
   },
+
+  render: function() {
+    var html = this.movieListTemplate({movie: this.model.toJSON()});
+    // console.log(this.model.toJSON()); // look at JSON
+    this.$el.html(html);
+
+    this.delegateEvents();
+    return this;
+  }
 });
+
+export default MovieView;
