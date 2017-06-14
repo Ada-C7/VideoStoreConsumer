2edbb1e20612e6bb4c3c780f0bcbca642cb4c3cb
janicewilson <janice.wilson@mac.com>
2017-06-14 15:35:04 -0700

    Added files for db-model, db-views and db-collection.

---
+++ src/collections/db_movies.js
@@ -0,0 +1,9 @@
+import Backbone from 'backbone';
+import DBMovie from '../models/db_movie.js';
+
+var DBMovies = Backbone.Collection.extend({
+  model: DBMovie,
+  url: 'http://localhost:3000/movies?query='
+});
+
+export default DBMovies;
