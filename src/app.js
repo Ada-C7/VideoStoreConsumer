90d4bfd6f73081b71b7f0e6936410c020cc6eec9
Alix Hamilton <alix.hamilton@gmail.com>
2017-06-14 14:45:19 -0700

    shows all movies from API

--- src/app.js
+++ src/app.js
@@ -5,25 +5,8 @@ import MovieView from './views/movie_view.js';
 import MovieList from './collections/movie_list.js';
 import MovieListView from './views/movie_list_view.js';
 
-var movieData = [
-  {
-    title: "Psycho",
-    overview: "When larcenous real estate clerk Marion Crane goes on the lam with a wad of cash and hopes of starting a new life, she ends up at the notorious Bates Motel, where manager Norman Bates cares for his housebound mother. The place seems quirky, but fine… until Marion decides to take a shower.",
-    release_date: "1960-06-16",
-    image_url: "http://lorempixel.com/185/278/"
-  },
-  {
-    title: "Jaws",
-    overview: "An insatiable great white shark terrorizes the townspeople of Amity Island, The police chief, an oceanographer and a grizzled shark hunter seek to destroy the bloodthirsty beast.",
-    release_date: "1975-06-19",
-    image_url: "http://lorempixel.com/185/278/"
-  }
-];
-
-var movieList = new MovieList(movieData);
-// movieList.fetch();
-
-console.log(">>> Breadcrumbs #1 (Movie list collection creation)");
+var movieList = new MovieList();
+movieList.fetch();
 
 $(document).ready(function() {
   var movieListView = new MovieListView({
--- src/views/movie_list_view.js
+++ src/views/movie_list_view.js
@@ -8,10 +8,8 @@ var MovieListView = Backbone.View.extend({
   initialize: function(params) {
     this.template = params.template;
     this.listenTo(this.model, 'update', this.render);
-    console.log(">>> Breadcrumbs #2 (Movie list view init)");
   },
   render: function() {
-    console.log(">>> Breadcrumbs #3 (Movie list render start)");
     this.$('#movie-list').html('<ul></ul>');
     var that = this;
 
@@ -23,7 +21,6 @@ var MovieListView = Backbone.View.extend({
       });
       that.$('#movie-list ul').append(movieView.render().$el);
     });
-    console.log(">>> Breadcrumbs #4 (Movie list render end)");
     return this;
   }
 });
