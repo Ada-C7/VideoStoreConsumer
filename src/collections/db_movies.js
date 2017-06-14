e786f7916faaaffeb188eed1198d96b50dccf150
janicewilson <janice.wilson@mac.com>
2017-06-14 15:52:20 -0700

    Got search working without commingling collections.

--- src/collections/db_movies.js
+++ src/collections/db_movies.js
@@ -3,7 +3,7 @@ import DBMovie from '../models/db_movie.js';
 
 var DBMovies = Backbone.Collection.extend({
   model: DBMovie,
-  url: 'http://localhost:3000/movies?query='
+  url: 'http://localhost:3000/movies'
 });
 
 export default DBMovies;
--- src/views/db_movies_view.js
+++ src/views/db_movies_view.js
@@ -6,14 +6,11 @@ import DBMovieView from './db_movie_view.js';
 import DBMovie from '../models/db_movie.js';
 
 
-
-
 var DBMoviesView = Backbone.View.extend({
      initialize: function( params ) {
           this.template = params.stockTemplate;
           this.listenTo(this.model, 'update', this.render);
      },
-
      render: function() {
 
          this.$( '#movie-results' ).empty();
@@ -28,23 +25,24 @@ var DBMoviesView = Backbone.View.extend({
           that.$('#movie-results').append( dbMovieView.render().$el );
           });
           return this;
+     },
+     events:  {
+          'click #search-button' : 'searchMovie'
+     },
+     getFormData: function() {
+          var formQuery = this.$('#query').val();
+          this.$('#query').val('');
+          return formQuery;
+     },
+     searchMovie: function() {
+          console.log('test');
+          event.preventDefault();
+          
+          this.model.fetch({
+          data: {query: this.getFormData()}
+          });
+
      }
-     // events:  {
-     //      'click #search-button' : 'searchMovie'
-     // },
-     // // getFormData: function() {
-     // //      var formQuery = this.$('#query').val();
-     // //      this.$('#query').val('');
-     // //      return formQuery;
-     // // },
-     // searchMovie: function() {
-     //      event.preventDefault();
-     //
-     //      this.model.fetch({
-     //      data: {query: this.getFormData()}
-     //      });
-     //
-     // }
 });
 
 export default DBMoviesView;
