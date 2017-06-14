5bc4af8d56a27ec133b0417262c72e52171b208a
sai samant <saisamant@gmail.com>
2017-06-14 16:22:59 -0700

    WIP render search results in same view as movie collection

--- src/views/movie_list_view.js
+++ src/views/movie_list_view.js
@@ -9,7 +9,9 @@ var MovieListView = Backbone.View.extend ({
   initialize: function(params) {
     this.template = params.template;
     this.listenTo(this.model, 'update', this.render);
+    this.listenTo(this.model, 'change', this.render);
   },
+
   render: function() {
     this.$('#movie-list').empty();
     var that = this;
@@ -22,33 +24,43 @@ var MovieListView = Backbone.View.extend ({
       that.$('#movie-list').append(movieView.render().$el);
     });
   },
+
   events: {
     "click #search-movies" : "searchMovie"
   },
+
   getFormData: function() {
     var formTitle = this.$("#title").val();
     this.$("#title").val('');
 
     return formTitle;
   },
+
   searchMovie: function() {
-    console.log("hello");
+
+
     // var searchTerm = this.getFormData();
     var searchMovieList = new MovieList();
-    console.log(searchMovieList);
     searchMovieList.fetch({ error: function() {console.log(arguments);},
       traditional: true,
       data: {query: [this.getFormData()]}
     });
-    console.log(this.getFormData());
 
-    var searchMovieListView = new MovieListView({
-      model: searchMovieList,
-      template: _.template($('#search-list-template').html()),
-      el: 'main'
-    });
-    searchMovieList.render();
+    this.model = searchMovieList;
+    console.log(this.model);
+    this.template = _.template($('#search-list-template').html());
+    console.log(this.template);
+    myMovieListView.render();
+
+    // var searchMovieListView = new MovieListView({
+    //   model: searchMovieList,
+    //   template: _.template($('#search-list-template').html()),
+    //   el: 'main'
+    // });
+    //
+    // searchMovieList.render();
   }
+
 
 });
 
