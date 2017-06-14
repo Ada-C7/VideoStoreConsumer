bf4760b37d5ca88a0c36f9c28d17c9dbcb709f8c
Ann Dai <gofarann@gmail.com>
2017-06-14 11:03:59 -0700

    changed some syntax on app.js, added rental lib view

--- src/app.js
+++ src/app.js
@@ -7,24 +7,21 @@ import MovieView from 'app/collections/movie_view';
 import RentalLibrary from 'app/views/rental_library.js';
 
 
-var movieView = new MovieView();
+var rentalLibrary = new RentalLibrary();
 // movieView.fetch();
 
 
-var movieListView = new MovieListView({
-  model: movieList,
+// would be replaced with fetched rental library from rails api
+var rentalLibraryView = new RentalLibraryView({
+  model: rentalLibrary,
   template:
+  // tempalte would be template to display all movies from rental library
   _.template($('#movie-card-template').html()),
   el: 'main'
 });
-movieListView.render();
-// what is this?
-  $('section.main-content').append('<p>Hello World!</p>');
-
 
 
 
 $(document).ready(function() {
-
-
+  rentalLibraryView.render();
 });
---
+++ src/views/rental_library_view.js
@@ -0,0 +1,8 @@
+import Backbone from 'backbone';
+import _ from 'underscore';
+import $ from 'jquery';
+
+import RentalLibrary from '../collections/rental_library';
+
+
+export default RentalLibraryView;
