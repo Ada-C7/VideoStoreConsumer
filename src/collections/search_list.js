c6fb04206f892b5d557dbed96d267348532463fa
Chantal Rollison <rollison.chantal@gmail.com>
2017-06-14 13:21:19 -0700

    search button alive, but not working. Need to write fetch function

--- src/collections/search_list.js
+++ src/collections/search_list.js
@@ -1,10 +1,9 @@
 import Backbone from 'backbone';
-
 import Video from '/models/video';
 
 var SearchVideoList = Backbone.Collection.extend({
   model: Video,
-  url: 'http://localhost:3000/movies'
+  url: url
 });
 
 export default VideoList;
--- src/views/video_list_view.js
+++ src/views/video_list_view.js
@@ -24,39 +24,20 @@ var VideoListView = Backbone.View.extend({
     });
     return this;
   },
-  // events: {
-  //   "click #add-video": "addVideo"
-  // },
-  // getFormData: function() {
-  //   var formName =
-  //   this.$("#name").val();
-  //   this.$("#name").val('');
-  //
-  //   var formAge =
-  //   this.$("#age").val();
-  //   this.$("#age").val('');
-  //
-  //   var formBreed =
-  //   this.$("#breed").val();
-  //   this.$("#breed").val('');
-  //
-  //   // returns a boolean
-  //   var formVaccinated =
-  //   // figure out if it's checked
-  //   this.$('#vaccinated-checkbox').is(":checked");
-  //   // clear checkbox
-  //   this.$('#vaccinated-checkbox').prop('checked', false);
-  //
-  //   // returns a new JS object
-  //   return {
-  //     name: formName,
-  //     age: formAge,
-  //     breed: formBreed,
-  //     vaccinated: formVaccinated
-  //   };
-  // },
-  addVideo: function() {
-    var video = new Video(this.getFormData());
+  events: {
+    "click #search-video": "searchVideo"
+  },
+  getFormData: function() {
+    var formTitle =
+    this.$("#title").val();
+    this.$("#title").val('');
+
+    return {
+      title: formTitle,
+    };
+  },
+  searchVideo: function() {
+    // var video = new Video(this.getFormData());
     // add to the collection
     this.model.create(video); // the model is the collection for the video list
   }
