3a3a0c9f0a77139456fa2a2d8825e3c5bd726420
Anacelia Gillette <acgillette426@gmail.com>
2017-06-15 09:39:13 -0700

    Made customer template and customer view skeleton

---
+++ src/views/customer_view.js
@@ -0,0 +1,10 @@
+import Backbone from 'backbone';
+import Customer from '../models/movie.js';
+import $ from 'jquery';
+import _ from 'underscore';
+
+var CustomerView = Backbone.View.extend({
+
+});
+
+export default CustomerView;
