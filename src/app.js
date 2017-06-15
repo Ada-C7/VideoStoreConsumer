702b1d5dc7f2223f75fd4f329e599e7600f9728f
Anacelia Gillette <acgillette426@gmail.com>
2017-06-15 10:10:43 -0700

    Got customers to show

--- src/app.js
+++ src/app.js
@@ -5,6 +5,12 @@ import Movies from './collections/movies.js';
 import MovieView from './views/movie_view.js';
 import MoviesView from'./views/movies_view.js';
 
+import Customer from './models/customer.js';
+import Customers from './collections/customers.js';
+import CustomerView from './views/customer_view.js';
+import CustomersView from'./views/customers_view.js';
+
+
 // Import jQuery & Underscore
 import $ from 'jquery';
 import _ from 'underscore';
@@ -24,6 +30,17 @@ $(document).ready(function() {
 
   moviesView.render();
 
+  $('.view-customers').click(function() {
+    var customerList = new Customers();
+    customerList.fetch();
+    // ready to go
+
+    var customersView = new MoviesView({
+      model: customerList,
+      template: _.template($("#customer-template").html()),
+      el: 'main'
+    });
+  });
 
 
 
