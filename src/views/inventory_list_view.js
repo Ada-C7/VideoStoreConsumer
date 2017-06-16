c003c7da297f99e7f6e979c7f30f773e53f26898
Ting Wong <hellotingwong@gmail.com>
2017-06-16 09:27:53 -0700

    added alert and fixed index.html

--- src/views/inventory_list_view.js
+++ src/views/inventory_list_view.js
@@ -44,6 +44,7 @@ var InventoryListView = Backbone.View.extend({
     console.log(existingMovie);
     if (existingMovie == null) {
       this.model.create(movie);
+      alert("You have successfully added this movie")
     }
     else{
       alert("You have already added this movie")
