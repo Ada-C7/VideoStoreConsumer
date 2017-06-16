04bddff4f10e47b90fb0de54e296624f0ed9bcba
Bo Trethewey <botrethewey@gmail.com>
2017-06-16 09:40:50 -0700

    removed unused files

--- src/collections/library.js
+++
@@ -1,9 +0,0 @@
-import Backbone from 'backbone';
-import Movie from '../models/movie.js';
-
-var Library = Backbone.Collection.extend({
-  model: Movie,
-  url: "http://localhost:3000/movies"
-});
-
-export default Library;
--- src/collections/search.js
+++
@@ -1,9 +0,0 @@
-import Backbone from 'backbone';
-import Movie from '../models/movie.js';
-
-var Search = Backbone.Collection.extend({
-  model: Movie,
-  url: "http://localhost:3000/movies?query="
-});
-
-export default Search;
