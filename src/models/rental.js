515b0d322cfa789919749b91ad66597743713351
Lynn Trickey <lynn.trickey@gmail.com>
2017-06-19 13:55:57 -0700

    committing before switching branches

--- src/models/rental.js
+++ src/models/rental.js
@@ -4,22 +4,11 @@ import $ from 'jquery';
 
 var Rental = Backbone.Model.extend({
   url: 'http://localhost:3000/rentals/',
-//   sync: function (method, model, options) {
-//   options.data = _.pick(this.attributes, 'foo', 'bar', 'baz');
-//   return Backbone.sync.call(this, method, model, options);
-// }
   sync: function(method, model, options) {
     if (method == "create") {
-      // console.log("inside sync!");
       method = 'check-out';
-      console.log(options.customer_id);
-      // options.url =  this.url + this.get('title') + '/check-out';
-      // // this = this.toJSON();
       options.data = JSON.stringify(this.attributes);
       options.contentType = 'application/json';
-      // options.data['due_date'] = "1/1/19";
-      console.log(options.data);
-      // console.log(options.url);
       return Backbone.sync(method, model, options);
     }
   }
