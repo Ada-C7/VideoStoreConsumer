9e9f1640ac984a0e5f5dec55ba894ac612ef5090
Olivia <ssecretsharer@gmail.com>
2017-06-19 13:32:38 -0700

    first movie model

---
+++ src/models/movie.js
@@ -0,0 +1,13 @@
+import Backbone from 'backbone';
+
+var Movie = Backbone.Model.extend({
+
+    initialize: function(params) {
+
+},
+//url is the default route tht fetch goes to on the model
+ url: 'http://localhost:3000/movies'
+
+});
+
+export default Movie;
