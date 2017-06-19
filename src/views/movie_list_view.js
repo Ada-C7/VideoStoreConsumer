1af4d82836724dedbc2937bee5b83eefbfee0780
Aurora Lemieux <aurora.lemieux@gmail.com>
2017-06-19 10:48:37 -0700

    fixed bug where columns change if search while details shown

--- src/views/movie_list_view.js
+++ src/views/movie_list_view.js
@@ -6,6 +6,7 @@ import Movie from '../models/movie.js';
 
 const MovieListView = Backbone.View.extend({
   initialize: function(params) {
+    this.details = false;
     this.template = params.template;
     this.model = params.model.movie;
     this.search = params.model.search;
@@ -13,8 +14,6 @@ const MovieListView = Backbone.View.extend({
     this.$("#search-header-section").hide();
     this.$("#library-header-section").hide();
     this.$("#movie-details").hide();
-    this.$("#list-main").removeClass("large-9");
-    this.$("#list-main").addClass("large-12");
   },
 
   resetHeaders: function() {
@@ -59,6 +58,10 @@ const MovieListView = Backbone.View.extend({
       that.listenTo(movieView, "showMovie", that.showMovieDetails);
     });
     this.resetButtons();
+    if (this.details === true) {
+      this.$(".movie-card").removeClass("large-3");
+      this.$(".movie-card").addClass("large-4");
+    }
     return this;
   },
 
@@ -71,10 +74,7 @@ const MovieListView = Backbone.View.extend({
   getSearch: function() {
     this.model.url = "http://localhost:3000/movies";
     console.log("clicked search button");
-    console.log(this.model.url);
-    console.log(this.search);
     this.search = true;
-    console.log(this.search);
     var text= this.$("#query").val();
     this.model.query = text;
     this.model.url += "?query=" + this.$("#query").val();
@@ -86,15 +86,12 @@ const MovieListView = Backbone.View.extend({
         console.log("Failure", data);
       }
     });
-    console.log(this.model.url);
-
   },
 
   showMovieDetails: function(movie) {
+    this.details = true;
     console.log("inside showMovieDetails");
-    console.log(movie.model);
     this.$("#movie-details").show();
-    this.$("#list-main").removeClass("large-12");
     this.$("#list-main").addClass("large-9");
     this.$(".movie-card").removeClass("large-3");
     this.$(".movie-card").addClass("large-4");
@@ -109,8 +106,8 @@ const MovieListView = Backbone.View.extend({
   },
 
   hideMovieDetails: function() {
+    this.details = false;
     this.$("#movie-details").hide();
-    this.$("#list-main").addClass("large-12");
     this.$("#list-main").removeClass("large-9");
     this.$(".movie-card").addClass("large-3");
     this.$(".movie-card").removeClass("large-4");
